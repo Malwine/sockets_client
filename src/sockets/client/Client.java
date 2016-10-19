d84df0b707c61a1505c3ea0ad1440d34e3060189
Malwine <malwine@mailbox.org>
2016-10-19 13:22:15 +0200

    Set up client class

---
+++ src/sockets/client/Client.java
@@ -0,0 +1,31 @@
+package sockets.client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+
+public class Client {
+	
+	public static void client() throws IOException {
+		Socket client =  new Socket(InetAddress.getLocalHost(), 8080);
+
+		OutputStream outputStream = client.getOutputStream();
+
+		outputStream.write("Hello World!".getBytes());
+
+		client.close();
+	}
+
+	public static void main(String[] args) {
+		
+		try {
+			client();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
